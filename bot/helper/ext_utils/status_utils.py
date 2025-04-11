0123db323db1c52446c03733cca5378e33a5306e
Noobie <155804401+noobietele@users.noreply.github.com>
2025-04-11 23:49:59 +0530

    Update status_utils.py

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -203,7 +203,7 @@ async def get_readable_message(
         status="All",
         page_step=1
     ):
-    msg = "<a href='https://t.me/JetMirror'>𝑩𝒐𝒕 𝒃𝒚 🚀 𝑱𝒆𝒕-𝑴𝒊𝒓𝒓𝒐𝒓</a>\n"
+    msg = "<a href='https://t.me/MonarchCloudUpdates">🚀Bot By Monarch-Cloud</a>\n"
     button = None
 
     tasks = await sync_to_async(
@@ -264,7 +264,7 @@ async def get_readable_message(
         #     )
 
         msg += (
-            f"\n<pre>#Jet{index + start_position} ❤🚀...(Processing)</pre>\n"
+            f"\n<pre>#Monarch{index + start_position} ❤🚀...(Processing)</pre>\n"
             f"Filename: {escape(f"{task.name()}")}\n"
         )
         if tstatus not in [
